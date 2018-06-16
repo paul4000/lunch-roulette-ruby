76923bad886818c7e7a6371ab774c3bb77d7e091
Paulina <pk.koziol49@gmail.com>
2018-06-16 12:16:38 +0200

    Added registration form validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, :email_format => {:message => 'Wrong format email'}
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
 end
