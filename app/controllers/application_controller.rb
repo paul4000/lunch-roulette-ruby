7254dcba2eefc34d2a05bfec6399be95fe581db2
Marcin-Chojnacki <chojnacki.marcin@op.pl>
2018-06-17 21:29:37 +0200

    Add 401 and 404 handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActionController::RoutingError, with: :not_found
+  rescue_from ActionController::UnknownController, with: :not_found
+  rescue_from AbstractController::ActionNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from CanCan::AccessDenied, with: :redirect_to_login
+
+  def not_found 
+    respond_to do |format|
+      format.html { render 'errors/404', :layout => "application", :status => 404 }
+      format.xml  { head 404 }
+      format.any  { head 404 }
+    end
+  end  
 
   private
 
