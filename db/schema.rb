a35cc2ef62b1f4cac57fd11d6e01b9e47f4de04f
Paulina <pk.koziol49@gmail.com>
2018-06-18 00:33:41 +0200

    Added admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180616231306) do
+ActiveRecord::Schema.define(version: 20180617220019) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -43,8 +43,9 @@ ActiveRecord::Schema.define(version: 20180616231306) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false, null: false
   end
 
 end
