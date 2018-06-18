d119660f778d0f6b289810436299dd65fea7e385
Paulina <pk.koziol49@gmail.com>
2018-06-18 02:26:43 +0200

    Cosmetics

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe, optional: true
-  validates :unit, :inclusion => { :in => %w(kilo teaspoon liter pinch),
+  validates :unit, :inclusion => { :in => %w(kilo teaspoon liter pinch cup),  allow_blank: true,
                                    :message => "%{value} is not a valid unit" }
 end
